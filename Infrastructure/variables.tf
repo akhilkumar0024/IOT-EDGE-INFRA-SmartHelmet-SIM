45a7695159633e3ee77ca0712aaa2c5201584527
akhilklumar0024 <akhilkumar0024.devops@gmail.com>
2026-06-08 13:45:17 +0000

    corrected removed  from type = string

--- Infrastructure/variables.tf
+++ Infrastructure/variables.tf
@@ -1,17 +1,17 @@
 variable "region" {
-  type        = "string"
+  type        = string
   description = "AWS Region"
   default     = "ap-south-1"
 }
 
 variable "environment" {
-  type        = "string"
+  type        = string
   description = "Environment"
   default     = "dev"
 }
 
 variable "project_name" {
-  type        = "string"
+  type        = string
   description = "Project Name"
   default     = "smart-helmet-infra"
 }
