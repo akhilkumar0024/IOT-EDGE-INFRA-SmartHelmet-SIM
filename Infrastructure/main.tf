de63d47ea96f59a9a7d9d9ed698dca39af9e8bfa
akhilklumar0024 <akhilkumar0024.devops@gmail.com>
2026-07-26 03:46:34 +0000

    formatted main.tf

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -10,9 +10,9 @@ module "ecs-security-group" {
 }
 
 module "database" {
-  source                  = "./modules/database"
-  hot-storage-name        = var.hot-storage-name
-  cold-storage-name       = var.cold-storage-name
+  source                      = "./modules/database"
+  hot-storage-name            = var.hot-storage-name
+  cold-storage-name           = var.cold-storage-name
   execution-registry-name     = var.execution-registry-name
   device-status-db-table-name = var.device-status-db-table-name
 }
@@ -23,36 +23,36 @@ module "messaging" {
 }
 
 module "compute" {
-  source                   = "./modules/compute"
-  telemetry-queue-name     = module.messaging.telemetry-queue-name
-  alert-queue-name         = module.messaging.alert-queue-name
-  override-queue-name      = module.messaging.override-queue-name
-  LWT-queue-name           = module.messaging.lwt-queue-name
-  control-queue-name       = module.messaging.control-queue-name
-  crash-queue-name         = module.messaging.crash-queue-name
-  public-subnet-ids        = module.networking.public-subnet-ids
-  telemetry-queue-arn      = module.messaging.telemetry-queue-arn
-  control-queue-arn        = module.messaging.control-queue-arn
-  crash-queue-arn          = module.messaging.crash-queue-arn
-  alert-queue-arn          = module.messaging.alert-queue-arn
-  LWT-queue-arn            = module.messaging.lwt-queue-arn
-  override-queue-arn       = module.messaging.override-queue-arn
-  telemetry-queue-url      = module.messaging.telemetry-queue-url
-  control-queue-url        = module.messaging.control-queue-url
-  crash-queue-url          = module.messaging.crash-queue-url
-  alert-queue-url          = module.messaging.alert-queue-url
-  LWT-queue-url            = module.messaging.lwt-queue-url
-  override-queue-url       = module.messaging.override-queue-url
-  hot-storage-arn          = module.database.hot-storage-arn
-  cold-storage-arn         = module.database.cold-storage-arn
-  execution-registry-arn   = module.database.execution-registry-arn
-  hot-storage-name         = module.database.hot-storage-name
-  cold-storage-name        = module.database.cold-storage-name
-  execution-registry-name  = module.database.execution-registry-name
-  telemetry-code-repo-url  = module.ecr.telemetry-code-repo-url
-  processing-code-repo-url = module.ecr.processing-code-repo-url
-  alert-code-repo-url      = module.ecr.alert-code-repo-url
-  ecs-security-group-id    = module.ecs-security-group.aws-sg-ecs-infra-id
+  source                           = "./modules/compute"
+  telemetry-queue-name             = module.messaging.telemetry-queue-name
+  alert-queue-name                 = module.messaging.alert-queue-name
+  override-queue-name              = module.messaging.override-queue-name
+  LWT-queue-name                   = module.messaging.lwt-queue-name
+  control-queue-name               = module.messaging.control-queue-name
+  crash-queue-name                 = module.messaging.crash-queue-name
+  public-subnet-ids                = module.networking.public-subnet-ids
+  telemetry-queue-arn              = module.messaging.telemetry-queue-arn
+  control-queue-arn                = module.messaging.control-queue-arn
+  crash-queue-arn                  = module.messaging.crash-queue-arn
+  alert-queue-arn                  = module.messaging.alert-queue-arn
+  LWT-queue-arn                    = module.messaging.lwt-queue-arn
+  override-queue-arn               = module.messaging.override-queue-arn
+  telemetry-queue-url              = module.messaging.telemetry-queue-url
+  control-queue-url                = module.messaging.control-queue-url
+  crash-queue-url                  = module.messaging.crash-queue-url
+  alert-queue-url                  = module.messaging.alert-queue-url
+  LWT-queue-url                    = module.messaging.lwt-queue-url
+  override-queue-url               = module.messaging.override-queue-url
+  hot-storage-arn                  = module.database.hot-storage-arn
+  cold-storage-arn                 = module.database.cold-storage-arn
+  execution-registry-arn           = module.database.execution-registry-arn
+  hot-storage-name                 = module.database.hot-storage-name
+  cold-storage-name                = module.database.cold-storage-name
+  execution-registry-name          = module.database.execution-registry-name
+  telemetry-code-repo-url          = module.ecr.telemetry-code-repo-url
+  processing-code-repo-url         = module.ecr.processing-code-repo-url
+  alert-code-repo-url              = module.ecr.alert-code-repo-url
+  ecs-security-group-id            = module.ecs-security-group.aws-sg-ecs-infra-id
   step_function_arn                = module.step-function.state_machine_arn
   reconciliation_step_function_arn = module.step-function.reconciliation_state_machine_arn
   ecs-log-group-name               = var.ecs-log-group-name
