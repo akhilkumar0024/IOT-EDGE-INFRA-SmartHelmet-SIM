5edadc59b0898502efc3f82b878c23aee06a0daa
akhilklumar0024 <akhilkumar0024.devops@gmail.com>
2026-08-07 14:26:38 +0000

    fomratted the code

--- Infrastructure/modules/monitoring/main.tf
+++ Infrastructure/modules/monitoring/main.tf
@@ -15,11 +15,15 @@ resource "aws_cloudwatch_metric_alarm" "telemetry_dlq_alarm" {
   alarm_name        = "smart-helmet-telemetry-dlq-alarm"
   alarm_description = "Alarm when Telemetry DLQ has messages"
 
+  # Trigger the alarm if the metric is > 0
   comparison_operator = "GreaterThanThreshold"
   threshold           = "0"
-  evaluation_periods  = "1"
+
+  # Look at the metric once every 60 seconds (1 period of 60s)
+  evaluation_periods = "1"
   period             = "60"
 
+  # The specific AWS Metric we are watching
   namespace   = "AWS/SQS"
   metric_name = "ApproximateNumberOfMessagesVisible"
   statistic   = "Sum"
@@ -28,10 +32,11 @@ resource "aws_cloudwatch_metric_alarm" "telemetry_dlq_alarm" {
     QueueName = var.telemetry-dlq-name
   }
 
+  # When the alarm goes off, send a message to the SNS
   alarm_actions = [aws_sns_topic.smart-helmet-infra-alerts.arn]
 }
 
-# 2. Cloudwatch Alarm For Control DLQ
+#2.Cloudwatch Alarm For Control DLQ
 resource "aws_cloudwatch_metric_alarm" "control_dlq_alarm" {
   alarm_name          = "smart-helmet-control-dlq-alarm"
   alarm_description   = "Alarm when Control DLQ has messages"
@@ -48,7 +53,7 @@ resource "aws_cloudwatch_metric_alarm" "control_dlq_alarm" {
   alarm_actions = [aws_sns_topic.smart-helmet-infra-alerts.arn]
 }
 
-# 3. Cloudwatch Alarm For LWT DLQ
+#3.Cloudwatch Alarm For LWT DLQ
 resource "aws_cloudwatch_metric_alarm" "lwt_dlq_alarm" {
   alarm_name          = "smart-helmet-lwt-dlq-alarm"
   alarm_description   = "Alarm when LWT DLQ has messages"
@@ -65,7 +70,7 @@ resource "aws_cloudwatch_metric_alarm" "lwt_dlq_alarm" {
   alarm_actions = [aws_sns_topic.smart-helmet-infra-alerts.arn]
 }
 
-# 4. Cloudwatch Alarm For Crash DLQ
+#4.Cloudwatch Alarm For Crash DLQ
 resource "aws_cloudwatch_metric_alarm" "crash_dlq_alarm" {
   alarm_name          = "smart-helmet-crash-dlq-alarm"
   alarm_description   = "Alarm when Crash DLQ has messages"
@@ -82,7 +87,7 @@ resource "aws_cloudwatch_metric_alarm" "crash_dlq_alarm" {
   alarm_actions = [aws_sns_topic.smart-helmet-infra-alerts.arn]
 }
 
-# 5. Cloudwatch Alarm For Alert DLQ
+#5.Cloudwatch Alarm For Alert DLQ
 resource "aws_cloudwatch_metric_alarm" "alert_dlq_alarm" {
   alarm_name          = "smart-helmet-alert-dlq-alarm"
   alarm_description   = "Alarm when Alert DLQ has messages"
@@ -99,7 +104,7 @@ resource "aws_cloudwatch_metric_alarm" "alert_dlq_alarm" {
   alarm_actions = [aws_sns_topic.smart-helmet-infra-alerts.arn]
 }
 
-# 6. Cloudwatch Alarm For Override DLQ
+#6.Cloudwatch Alarm For Override DLQ
 resource "aws_cloudwatch_metric_alarm" "override_dlq_alarm" {
   alarm_name          = "smart-helmet-override-dlq-alarm"
   alarm_description   = "Alarm when Override DLQ has messages"
@@ -116,7 +121,7 @@ resource "aws_cloudwatch_metric_alarm" "override_dlq_alarm" {
   alarm_actions = [aws_sns_topic.smart-helmet-infra-alerts.arn]
 }
 
-# 7. Cloudwatch Alarm For DynamoDB Hot Storage
+#7.Cloudwatch Alarm For DynamoDB Hot Storage
 resource "aws_cloudwatch_metric_alarm" "dynamoDB-hot-storage-alarm" {
   alarm_name          = "smart-helmet-dynamodb-hot-storage-alarm"
   comparison_operator = "GreaterThanThreshold"
@@ -124,7 +129,7 @@ resource "aws_cloudwatch_metric_alarm" "dynamoDB-hot-storage-alarm" {
   threshold           = 0
   alarm_description   = "Alarm when DynamoDB hot storage experiences Read or Write throttling"
   alarm_actions       = [aws_sns_topic.smart-helmet-infra-alerts.arn]
-  
+  # Expression to add the read and write throttling
   metric_query {
     id          = "e1"
     expression  = "m1 + m2"
@@ -132,6 +137,7 @@ resource "aws_cloudwatch_metric_alarm" "dynamoDB-hot-storage-alarm" {
     return_data = true
   }
 
+  # Fetch the Read Throttles
   metric_query {
     id = "m1"
     metric {
@@ -144,6 +150,7 @@ resource "aws_cloudwatch_metric_alarm" "dynamoDB-hot-storage-alarm" {
       }
     }
   }
+  # Fetch the Write Throttles
   metric_query {
     id = "m2"
     metric {
@@ -158,7 +165,7 @@ resource "aws_cloudwatch_metric_alarm" "dynamoDB-hot-storage-alarm" {
   }
 }
 
-# 8. Cloudwatch Alarm For DynamoDB Cold Storage
+#8.Cloudwatch Alarm For DynamoDB Cold Storage
 resource "aws_cloudwatch_metric_alarm" "dynamoDB-cold-storage-alarm" {
   alarm_name          = "smart-helmet-dynamodb-cold-storage-alarm"
   comparison_operator = "GreaterThanThreshold"
@@ -166,7 +173,7 @@ resource "aws_cloudwatch_metric_alarm" "dynamoDB-cold-storage-alarm" {
   threshold           = 0
   alarm_description   = "Alarm when DynamoDB cold storage experiences Read or Write throttling"
   alarm_actions       = [aws_sns_topic.smart-helmet-infra-alerts.arn]
-
+  # Expression to add the read and write throttling
   metric_query {
     id          = "e1"
     expression  = "m1 + m2"
@@ -174,6 +181,7 @@ resource "aws_cloudwatch_metric_alarm" "dynamoDB-cold-storage-alarm" {
     return_data = true
   }
 
+  # Fetch the Read Throttles
   metric_query {
     id = "m1"
     metric {
@@ -186,6 +194,7 @@ resource "aws_cloudwatch_metric_alarm" "dynamoDB-cold-storage-alarm" {
       }
     }
   }
+  # Fetch the Write Throttles
   metric_query {
     id = "m2"
     metric {
@@ -200,7 +209,7 @@ resource "aws_cloudwatch_metric_alarm" "dynamoDB-cold-storage-alarm" {
   }
 }
 
-# 9. Cloudwatch Alarm For DynamoDB Execution Registry
+#9.Cloudwatch Alarm For DynamoDB Execution Registry
 resource "aws_cloudwatch_metric_alarm" "dynamoDB-execution-registry-alarm" {
   alarm_name          = "smart-helmet-dynamodb-execution-registry-alarm"
   comparison_operator = "GreaterThanThreshold"
@@ -208,7 +217,7 @@ resource "aws_cloudwatch_metric_alarm" "dynamoDB-execution-registry-alarm" {
   threshold           = 0
   alarm_description   = "Alarm when DynamoDB execution registry experiences Read or Write throttling"
   alarm_actions       = [aws_sns_topic.smart-helmet-infra-alerts.arn]
-
+  # Expression to add the read and write throttling
   metric_query {
     id          = "e1"
     expression  = "m1 + m2"
@@ -216,6 +225,7 @@ resource "aws_cloudwatch_metric_alarm" "dynamoDB-execution-registry-alarm" {
     return_data = true
   }
 
+  # Fetch the Read Throttles
   metric_query {
     id = "m1"
     metric {
@@ -228,6 +238,7 @@ resource "aws_cloudwatch_metric_alarm" "dynamoDB-execution-registry-alarm" {
       }
     }
   }
+  # Fetch the Write Throttles
   metric_query {
     id = "m2"
     metric {
@@ -242,7 +253,7 @@ resource "aws_cloudwatch_metric_alarm" "dynamoDB-execution-registry-alarm" {
   }
 }
 
-# 10. ECS Cluster Log Group 
+#10 ECS Cluster Log Group 
 resource "aws_cloudwatch_log_group" "ecs_cluster_logs" {
   name              = var.ecs-log-group-name
   retention_in_days = 14
@@ -250,7 +261,7 @@ resource "aws_cloudwatch_log_group" "ecs_cluster_logs" {
 
 # 11. IOT Core Cloudwatch Logs
 resource "aws_cloudwatch_metric_alarm" "iot_core_throttling_alarm" {
-  alarm_name          = "smart-helmet-iot-core-throttling-alarm"
+  alarm_name          = "iot-core-rule-throttling-alarm"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 1
   metric_name         = "RuleMessageThrottled"
@@ -262,7 +273,7 @@ resource "aws_cloudwatch_metric_alarm" "iot_core_throttling_alarm" {
   alarm_actions       = [aws_sns_topic.smart-helmet-infra-alerts.arn]
 }
 
-# 12. Step Function Cloudwatch Metrics
+#12 Step Function Cloudwatch Metrics
 resource "aws_cloudwatch_metric_alarm" "step_function_failures_alarm" {
   alarm_name          = "smart-helmet-step-function-failures-alarm"
   comparison_operator = "GreaterThanThreshold"
@@ -323,7 +334,7 @@ resource "aws_cloudwatch_metric_alarm" "sqs_old_message_alarm" {
   }
 }
 
-# 14. Application Log Metric Filter for ERRORs
+#14 Application Log Metric Filter for ERRORs
 resource "aws_cloudwatch_log_metric_filter" "ecs_error_filter" {
   name           = "smart-helmet-ecs-error-filter"
   pattern        = "ERROR"
@@ -336,7 +347,7 @@ resource "aws_cloudwatch_log_metric_filter" "ecs_error_filter" {
   }
 }
 
-# 15. CloudWatch Alarm for the Application Error Filter
+#15 CloudWatch Alarm for the Application Error Filter
 resource "aws_cloudwatch_metric_alarm" "ecs_error_alarm" {
   alarm_name          = "smart-helmet-ecs-error-alarm"
   comparison_operator = "GreaterThanThreshold"
@@ -346,11 +357,11 @@ resource "aws_cloudwatch_metric_alarm" "ecs_error_alarm" {
   period              = 60
   statistic           = "Sum"
   threshold           = 5
-  alarm_description   = "Alarm when Python logs 'ERROR' more than 5 times in a minute"
+  alarm_description   = "Alarm when the Python application logs 'ERROR' more than 5 times in a minute"
   alarm_actions       = [aws_sns_topic.smart-helmet-infra-alerts.arn]
 }
 
-# 16. ECS Cluster Memory Utilization Alarm
+#16 ECS Cluster Memory Utilization Alarm (OOM Prevention)
 resource "aws_cloudwatch_metric_alarm" "ecs_memory_alarm" {
   alarm_name          = "smart-helmet-ecs-memory-alarm"
   comparison_operator = "GreaterThanThreshold"
@@ -360,7 +371,7 @@ resource "aws_cloudwatch_metric_alarm" "ecs_memory_alarm" {
   period              = 60
   statistic           = "Average"
   threshold           = 90
-  alarm_description   = "Alarm when ECS Cluster Memory goes above 90%"
+  alarm_description   = "Alarm when ECS Cluster Memory goes above 90% (OOM risk)"
   alarm_actions       = [aws_sns_topic.smart-helmet-infra-alerts.arn]
   dimensions = {
     ClusterName = "smart-helmet-cluster"
